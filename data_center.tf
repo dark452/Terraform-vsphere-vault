0a9d0fea4cc64f294f0bf90c1f5bb6171791c23a
dark452 <imunoz@redeshost.cl>
2023-08-17 09:30:43 -0400

    Add datacenter data source definitions

---
+++ data_center.tf
@@ -0,0 +1,33 @@
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+
+data "vsphere_datastore" "datastore" {
+  name          = var.vm_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_datastore" "isodatastore" {
+  name          = var.iso_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere-cluster
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = var.vsphere_pool
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_network" "network" {
+  name          = var.vm_network
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vm_template_name
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
